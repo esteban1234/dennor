abc3fefa6e275c951e8788aff76a2cbef052c315
esteban1234 <tva_libra@hotmail.com>
2017-10-10 12:42:32 -0500

    pagina lista

---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>SERVICIOS DE MANTENIMIENTO DENNOR</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <META NAME="Title" content="SERVICIOS DE MANTENIMIENTO DENNOR">
+    <META NAME="Author" CONTENT="[http://mantenimientodennor.com/]"/>
+    <META name="Description" content="El mantenimiento y reparación de maquinaria pesada se enfoca en el conjunto de operaciones que técnicos profesionales hacen sobre la maquinaria para que esta cumpla adecuadamente con los propósitos sobre las cuales fue construida."/>
+   <META NAME="Keywords" CONTENT="montacargas, Bobcat, Liebherr, Hyundai, Crown, Rosenbauer, Trio, Kubota,  mantenimiento de maquinaria pesada, excavadoras, excavadora hidraulica, excavadoras, rodillo compactador, alquiler de maquinaria pesada, venta de cargadores frontales, venta de maquinaria pesada, elevadores de carga, motoniveladoras, cargador frontal, alquiler de maquinaria, empresas de maquinaria pesada, alquiler de montacargas, montacagas electricos, maquinaria para construccion, venta de montacargas,reparacion de maquinaria pesada, mantenimiento de maquinarias pesadas, mantenimiento de montacargas, mantenimiento de maquinaria, maquinaria construccion, venta de montacargas usados, repuestos maquinaria pesada, montacargas de combustion, montacargas a combustion."/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+
+<div class="bannerh" id="INICIO">
+  <div class="fondo">
+    <header>
+      <figure class="logo pull-left">
+        <img src="img/denor.png" class="img-responsive" alt="DENNOR">
+      </figure>
+      <nav class=" menu pull-right">
+        <ul>
+          <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO<i class="fa fa-bars fa-1x pull-right" style="margin-top: 5%;" aria-hidden="true"></i></a>
+          <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS<i class="fa fa-users fa-1x pull-right" style="margin-top: 5%;" aria-hidden="true"></i></a>
+          <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS<i class="fa fa-briefcase fa-1x pull-right" style="margin-top: 5%;" aria-hidden="true"></i></a>
+          <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO<i class="fa fa-phone fa-1x pull-right" style="margin-top: 5%;" aria-hidden="true"></i></a>
+        </ul>
+      </nav>
+    </header>
+    
+    <p class="text-banner">MANTENIMIENTO DE MAQUINARIA <br> PESADA</p>
+    <div style="text-align: center; margin-top: 5%; margin-bottom: 5%;">
+      <a class="btn btn-primary btn-outline btn-lg btn-color" data-scroll href="#CONTACTO">CONTACTANOS</a>
+    </div>
+  </div>
+</div>
+
+
+
+  <span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+<section class="seccion1" id="NOSOTROS">
+  <p class="tit"><i class="fa fa-chevron-right" aria-hidden="true"></i> QUIENES SOMOS</p> <br> <br>
+  <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h3 style="color: #97D1DA;">HACEMOS LA DIFERENCIA</h3>
+            <p>Un buen numero de refacciones adecuadas a la demanda de nuestra sucursal, lineas teléfonicas para agendar una cita o solicitar presupuestos y un sitio web para llegar a cualquier parte de la republica y asi respaldar la disponibilidad en nuestro país, así como el rápido servicio de nuestros proveedores, hacen posible el poder satisfacer las necesidades del mercado mexicano en la mayoría de los casos en menos de 24 horas.</p>
+          </div>
+          <div class="col-md-6">
+            <h3 style="color: #97D1DA;">MISION</h3>
+            <p>Brindar un servicio integral comercializando refacciones y productos de alta calidad para todo tipo de industria a través de nuestra amplia red de distribución.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6">
+            <h3 style="color: #97D1DA;">NUESTROS PROVEEDORES</h3>
+            <p>Las marcas de los productos que distribuimos son fabricadas siguiendo rigurosos procedimientos de control de calidad, lo que nos permite ofrecer partes con estandares iguales a los del equipo original.</p>
+          </div>
+          <div class="col-md-6">
+            <h3 style="color: #97D1DA;">VISION</h3>
+            <p>Ser la empresa más eficiente, lider e innovadora en el suministro de refacciones y productos de alta calidad para todo tipo de industria.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6">
+            <h3 style="color: #97D1DA;">EL MEJOR TIEMPO DE ENTREGA</h3>
+            <p>Nuestro gran nivel del personal, apoyado en un sistema con tecnología inteligente para clasificación y ubicación de las refacciones, en nuestros almacenes, e incluso entre las existencias de nuestros proveedores, asegura la disponibilidad de las refacciones.</p>
+          </div>
+          <div class="col-md-6">
+            <h3 style="color: #97D1DA;">VALORES</h3>
+            <div>
+              <p class="puntos">
+                <i class="fa fa-check" aria-hidden="true"></i> Honestidad <br> 
+                <i class="fa fa-check" aria-hidden="true"></i> Lealtad <br>
+                <i class="fa fa-check" aria-hidden="true"></i> Responsabilidad 
+              </p>
+              <p class="puntos2">
+                <i class="fa fa-check" aria-hidden="true"></i> Servicio y Calidad <br>
+                <i class="fa fa-check" aria-hidden="true"></i> Rentabilidad <br>
+                <i class="fa fa-check" aria-hidden="true"></i> Trabajo en equipo
+              </p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+
+<section class="seccion2" id="SERVICIOS">
+  <div class="banners">
+    <div class="fondo2">
+      <h1 class="tit2"><i class="fa fa-chevron-right" aria-hidden="true"></i> SERVICIOS</h1>
+    </div>
+  </div>
+  <p class="tits" >DENNOR, brinda los servicios necesarios en la rama de la mineria, industrial y construccion para el funcionamiento óptimo de su equipo ligero, pesado o especializado; cuenta con personal capacitado y preparado para garantizar un servicio de la mas alta calidad.</p>
+  <br><br><br>
+  <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <figure class="imagen">
+              
+            </figure>
+            <h1 style="color: #fff; text-align: center;">Mantenimiento y Reparación</h1>
+            <p style="color: #fff; text-align: justify;">Ofrecemos el servicio de mantenimiento para su equipo ligero, pesado ó especializado manejando todas las marcas disponibles, entregando un analisis del estado de su equipo. <br>
+ 
+            Contamos con tecnicos capacitados, herramientas y equipos necesarios para el mantenimiento, diagnostico y reparacion de fallas con garantia de satisfaccion en nuestros trabajos, manejando refacciones originales, con el fin de ofrecerle la disponibilidad de su equipo.</p>
+          </div>
+          <div class="col-md-6">
+            <figure class="imagen2">
+              
+            </figure>
+            <h1 style="color: #fff; text-align: center;">Renta</h1>
+            <p style="color: #fff; text-align: justify;">Ofrecemos el servicio de renta de maquinaria pesada y ligera, siendo un aliado en sus proyectos y trabajos de su empresa. <br><br>
+ 
+            Tiene la opción de alquilar el equipo por horas ó días y si lo requiere, el operador del equipo.</p>
+          </div>
+        </div> <br><br>
+
+        <div class="row">
+          <div class="col-md-6">
+            <figure class="imagen3">
+              
+            </figure>
+            <h1 style="color: #fff; text-align: center;">Asesoria y Presupuesto</h1>
+            <p style="color: #fff; text-align: justify;">Ofrecemos el servicio de mantenimiento para su equipo ligero, pesado ó especializado manejando todas las marcas disponibles, entregando un analisis del estado de su equipo. <br>
+ 
+            Contamos con tecnicos capacitados, herramientas y equipos necesarios para el mantenimiento, diagnostico y reparacion de fallas con garantia de satisfaccion en nuestros trabajos, manejando refacciones originales, con el fin de ofrecerle la disponibilidad de su equipo.</p>
+          </div>
+          <div class="col-md-6">
+            <figure class="imagen4">
+              
+            </figure>
+            <h1 style="color: #fff; text-align: center;">Venta de Equipo</h1>
+            <p style="color: #fff; text-align: justify;">Ofrecemos el servicio de renta de maquinaria pesada y ligera, siendo un aliado en sus proyectos y trabajos de su empresa. <br><br>
+ 
+            Tiene la opción de alquilar el equipo por horas ó días y si lo requiere, el operador del equipo.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</section>
+
+<section class="seccion3" id="CONTACTO">
+  <div class="bannerc">
+    <div class="fondo2">
+      <h1 class="tit2"><i class="fa fa-chevron-right" aria-hidden="true"></i> CONTACTO</h1>
+    </div>
+  </div>
+  <div class="mapa">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.4209045515577!2d-93.18314268597845!3d16.755722088460022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecdbd7050a598b%3A0x58679dab396866c3!2sAv.+Fontana+374%2C+El+Campanario%2C+29057+Tuxtla+Guti%C3%A9rrez%2C+Chis.!5e0!3m2!1ses-419!2smx!4v1507654846563" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+
+  <div class="container">
+  <div class="row text-center">
+    <div class="col-sm-3 col-xs-6 first-box">
+        <h1><span class="fa fa-phone" aria-hidden="true"></span></h1>
+        <h3>Teléfono</h3>
+        <p>PENDIENTE</p><br>
+    </div>
+    <div class="col-sm-3 col-xs-6 second-box">
+        <h1><span class="fa fa-street-view" aria-hidden="true"></span></h1>
+        <h3>Dirección</h3>
+        <p>Av. Fontana #374 Col. El Campanario. Tuxtla Gutierrez, Chiapas. C.P. 29054</p><br>
+    </div>
+    <div class="col-sm-3 col-xs-6 third-box">
+        <h1><span class="fa fa-envelope-o" aria-hidden="true"></span></h1>
+        <h3>E-mail</h3>
+        <p>info@mantenimientodennor.com</p><br>
+    </div>
+    <div class="col-sm-3 col-xs-6 fourth-box">
+      <h1><span class="fa fa-clock-o" aria-hidden="true"></span></h1>
+        <h3>Horario</h3>
+        <p>Lunes a Viernes de 9 am a 6 pm</p><br>
+    </div>
+  </div>
+</div>
+<br><br>
+            <div class="row">
+                <div class="col-sm-6 col-centered">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" >
+                            <h1>Formulario de contacto</h1>
+                        </div>
+                    <form action="" method="POST">
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user blue"></i></span>
+                                <input type="text" name="InputName" placeholder="Nombre completo" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-envelope blue"></i></span>
+                                <input type="email" name="InputEmail" placeholder="Email" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-phone blue"></i></span>
+                                <input type="text" name="InputName" placeholder="Teléfono" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-comment blue"></i></span>
+                                <textarea name="InputMessage" rows="6" class="form-control" type="text" required></textarea>
+                            </div>
+                        </div>
+                        <div class="">
+                        <button type="submit" class="btn btn-info pull-right">ENVIAR <span class="fa fa-send"></span></button>
+                            <button type="reset" value="Reset" name="reset" class="btn">BORRAR <span class="fa fa-refresh"></span></button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</section>
+
+<footer>
+<p>Derechos de autor 2017 © | SERVICIOS DE MANTENIMIENTO DENNOR S.A. DE C.V.</p>
+</footer>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
